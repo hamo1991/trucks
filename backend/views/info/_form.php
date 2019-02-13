51b5946a79f8e45a656148a9e0f0e58fbd8be04b
Tea <tea@tea.77>
2019-02-13 13:13:50 +0400

    change models

---
+++ backend/views/info/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Info */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="info-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'type')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'content')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ common/models/Info.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "info".
+ *
+ * @property int $id
+ * @property string $type
+ * @property string $content
+ * @property string $slug
+ */
+class Info extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'info';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['type', 'content', 'slug'], 'required'],
+            [['type', 'content', 'slug'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'type' => 'Type',
+            'content' => 'Content',
+            'slug' => 'Slug',
+        ];
+    }
+}
